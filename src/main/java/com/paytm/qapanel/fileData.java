5ecc8fb6f865344181ee3b988180a0d51d9f41f9
manish mishra <manish1.mishra@paytm.com>
2019-01-31 23:39:36 +0530

    temp changes

--- src/main/java/com/paytm/qapanel/fileData.java
+++ src/main/java/com/paytm/qapanel/fileData.java
@@ -1,4 +1,28 @@
 package com.paytm.qapanel;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 
-public class fileData {
+import java.sql.*;
+import java.util.*;
+import java.util.Map.Entry;
+import java.io.*;
+
+public class fileData
+{
+
+   public static void main(String [] ghfsh) throws IOException, ConfigurationException {
+
+       FileReader reader=new FileReader("/Users/mainishmishra/Automation_Repo/paytm-test/src/main/resources/application.properties");
+       Properties p=System.getProperties();
+       p.load(reader);
+       HashMap<String, String> value_from_file=new HashMap<>();
+       for (final Map.Entry<Object, Object> entry : p.entrySet()) {
+           value_from_file.put((String) entry.getKey(), (String) entry.getValue());
+       }
+       System.out.println(value_from_file.keySet());
+
+       PropertiesConfiguration properties = new PropertiesConfiguration("/Users/mainishmishra/Automation_Repo/paytm-test/src/main/resources/application.properties");
+
+       //System.out.println(properties.);
+   }
 }
--- src/main/java/com/paytm/qapanel/updateProperty.java
+++ src/main/java/com/paytm/qapanel/updateProperty.java
@@ -1,4 +1,20 @@
 package com.paytm.qapanel;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+public class updateProperty
+{
+    public static void main(String[] args) {
+        try {
+            PropertiesConfiguration properties = new PropertiesConfiguration("/Users/mainishmishra/Automation_Repo/paytm-test/src/main/resources/application.properties");
+            properties.setProperty("Qa7_IP", "QA7 IP changed.");
+            //properties.setProperty("jdbc.url", "jdbc:mysql://localhost:3306/technicalkeeda");
+            //properties.setProperty("jdbc.username", "root");
+            //properties.setProperty("jdbc.password", "password");
+            properties.save();
+            System.out.println("config.properties updated Successfully!!");
+        } catch (ConfigurationException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
-public class updateProperty {
 }
